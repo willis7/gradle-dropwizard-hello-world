da67e9917752218b6c9ac2aed0ae8e9afc57095a
Sion Williams <sion5@hotmail.co.uk>
2014-10-26 21:19:15 +0000

    Validating configuration settings

--- src/main/java/com/github/willis/HelloWorldConfiguration.java
+++ src/main/java/com/github/willis/HelloWorldConfiguration.java
@@ -2,15 +2,21 @@ package com.github.willis;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Max;
 
 /**
  * @author Sion Williams
  */
 public class HelloWorldConfiguration extends Configuration {
     @JsonProperty
+    @NotEmpty
     private String message;
 
     @JsonProperty
+
+    @Max( 10 )
     private int messageRepetitions;
 
     public String getMessage() {
